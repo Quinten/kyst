4f238c761dedf4b292213bb394f06753cc25603e
Quinten Clause <quintenclause@hotmail.com>
2021-10-01 21:23:23 +0200

    Revert fillrect

--- components/fillrect.js
+++ components/fillrect.js
@@ -1,11 +1,5 @@
 let draw = c => {
     let {ctx} = c;
-    ctx.restore();
-    ctx.save();
-    let {zoom} = c.entities.viewport.resize;
-    ctx.scale(zoom, zoom);
-    let {dragcamera} = c.entities.camera;
-    ctx.translate(dragcamera.x, dragcamera.y);
     let {x, y, width, height, fill} = c.component;
     ctx.fillStyle = fill;
     ctx.fillRect(x, y, width, height);
